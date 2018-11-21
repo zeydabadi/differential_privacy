9a7cd7f3c33e034b3604bb3084fce04f328823c6
zeydabadi@gmail.com <zeydabadi@gmail.com>
2018-11-20 21:21:48 -0500

    second commit

---
+++ diff_privacy.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 20:11:25 2018
+
+@author: Mahmoud Zeydabadinezhad
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+def diff_privacy(File, Ep):
+    """
+    Inputs: 
+        - File: Path to the CSV file
+        - ep:   The value of epsilon
+    
+    Outputs: 
+        - The original histogram and error plot.
+    """
+    print(Ep)
+    df = pd.read_csv(File)
+    MinAge = df.Age.min()
+    MaxAge = df.Age.max()
+    Bins = range(MinAge,MaxAge+1,5)
+    counts, bins = np.histogram(df.Age, bins=Bins)
+    #print(counts)
+    #print(bins)
+    def laplaceMechanism(x, epsilon):
+        x +=  np.random.laplace(0, epsilon)
+        return x
+    
+    laplaceMechanism_v = np.vectorize(laplaceMechanism)
+    err_list  = []
+    new_count = []
+    EP = np.linspace(0.1,1,10)
+    
+    for ep in EP:
+        countsPrime = laplaceMechanism_v(counts,1/ep)
+        new_count.append(countsPrime)
+        error = counts - countsPrime
+        err_list.append(np.mean(np.abs(error)))
+        
+        
+    fig, ax = plt.subplots(1, 2, figsize=(20, 18))
+    colors = ['red', 'blue']
+    axes = ax.flatten()
+    axes[0].hist(df.Age, bins=Bins)
+    axes[0].set_title('Age histogram',fontsize='large')
+    axes[0].set_ylabel('Number of people', color='blue')
+    axes[0].set_xlabel('Age', color='blue')
+    axes[0].grid(True)
+    axes[0].legend(loc='upper right')
+        
+    axes[1].set_title('Error plot',fontsize='large')
+    axes[1].plot(EP,err_list)
+    axes[1].set_ylabel('Absolute error')
+    axes[1].set_xlabel('epsilon')
+    axes[1].set_xticks(EP)
+    axes[1].grid(True)
+    plt.show()
+
+if __name__== "__main__":
+    if len(sys.argv) == 3:
+        diff_privacy(sys.argv[1],sys.argv[2])
+    else:
+        sys.exit("\nUsage: diff_privacy.py path_to_CSV_file epsiln\n\n\n\n")
